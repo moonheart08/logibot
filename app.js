bc37aef224af6a6d4b6312f3da716799fd4cc076
Moon <bradenanelson@gmail.com>
2017-03-07 00:00:01 +0000

    y

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ async function init() {
             commands[cmd.toLowerCase()](parsed,client);
         }
     });
-    
+    console.log((await fs.readFileAsync(`${__dirname}/token`)).toString());
     await client.login((await fs.readFileAsync(`${__dirname}/token`)).toString());
     console.log(client.user.id);
     
